ecb922c745ce8f2ed551ab783c4f9fc171e8ade3
Dmitriy <93316340+dmitriy-paranin@users.noreply.github.com>
2021-10-31 18:47:57 +0300

    Add files via upload

---
+++ Basic_JavaScript_85-113.js
@@ -0,0 +1,21 @@
+/*
+Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+const entreeValue = testObj;   
+const drinkValue = testObj;    
+*/
+
+//My solution
+
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+const entreeValue = testObj["an entree"];   
+const drinkValue = testObj["the drink"];    
