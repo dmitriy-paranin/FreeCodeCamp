2abe6b29ba1b72ccf381628e2b4b5a8a09c9d21f
Dmitriy <93316340+dmitriy-paranin@users.noreply.github.com>
2021-10-31 18:32:27 +0300

    Add files via upload

---
+++ Basic_JavaScript_83-113.js
@@ -0,0 +1,12 @@
+/*
+You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+*/
+
+//My solution
+
+const myDog = {
+name: "Dima",
+legs: 2,
+tails: 0,
+friends: ["Anton", "Vadim", "Igor"]
+};
