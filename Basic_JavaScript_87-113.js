e7baf5862b5d01f51c044a6e37929826cbb870f1
Dmitriy <93316340+dmitriy-paranin@users.noreply.github.com>
2021-10-31 19:29:45 +0300

    Add files via upload

---
+++ Basic_JavaScript_87-113.js
@@ -0,0 +1,20 @@
+/*
+Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.
+
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+}; 
+*/
+
+//My solution
+
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = "Happy Coder";
