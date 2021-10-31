a85c9807036007335bf1b01235154c2d367520c9
Dmitriy <93316340+dmitriy-paranin@users.noreply.github.com>
2021-10-31 09:16:04 +0300

    Add files via upload

---
+++ Basic_JavaScript_80-113.js
@@ -0,0 +1,23 @@
+/*
+Fix the function isLess to remove the if/else statements.
+
+function isLess(a, b) {
+  // Only change code below this line
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+  // Only change code above this line
+}
+
+isLess(10, 15);
+*/
+
+//My solution
+
+function isLess(a, b) {
+      return a < b;
+ }
+
+isLess(10, 15);
